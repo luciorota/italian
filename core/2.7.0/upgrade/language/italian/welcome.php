68c25b6f61e3052601716f74cf25cab0203be2a7
Lucio Rota <lucio.rota@gmail.com>
2026-05-11 08:32:29 +0200

    Update welcome.php

--- core/2.7.0/upgrade/language/italian/welcome.php
+++ core/2.7.0/upgrade/language/italian/welcome.php
@@ -10,12 +10,12 @@ define(
 <h2>XOOPS Aggiornamento</h2>
 
 <p>
-<em>Aggiornamento</em> esaminerà questa installazione XOOPS e applicherà tutte le patch necessarie per renderla compatibile 
+<em>Aggiornamento</em> esaminer&agrave; questa installazione XOOPS e applicher&agrave; tutte le patch necessarie per renderla compatibile 
 con il nuovo codice XOOPS. Le patch possono includere modifiche al database, aggiungendo impostazioni predefinite per i nuovi
 elementi di configurazione, aggiornamenti di file e dati e altro ancora.
 <p>
-Dopo ogni patch, il programma di aggiornamento riporterà lo stato e attenderà il tuo input per continuare. Al
-Al termine dell'aggiornamento, il controllo passerà alla funzione di aggiornamento del modulo di sistema.
+Dopo ogni patch, il programma di aggiornamento riporter&agrave; lo stato e attender&agrave; il tuo input per continuare. Al
+Al termine dell'aggiornamento, il controllo passer&agrave; alla funzione di aggiornamento del modulo di sistema.
 
 <div class="alert alert-warning">
 Una volta completato l'aggiornamento, non dimenticare di:
